d378cc2d9f75a6bcfb9ae66c0fa1b8badab7d15f
lyd111 <2082885180@qq.com>
2020-11-01 21:19:18 +0800

    本地仓库接收

---
+++ ssm/src/com/liyingdong/git/Demo.java
@@ -0,0 +1,13 @@
+package com.liyingdong.git;
+
+/**
+ * @author 李瀛东
+ * @site www.xiaomage.com
+ * @company xxx公司
+ * @create  2020-11-01 21:16
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("idea git demo");
+    }
+}
